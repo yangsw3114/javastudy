9b70c361ff6a0f6ff696ea06e6b3180420fc2d75
yangsw3114 <ysw3114@naver.com>
2021-09-06 19:58:31 +0900

    두번쨰 커밋

--- practice01/src/main/java/prob2/Prob2.java
+++ practice01/src/main/java/prob2/Prob2.java
@@ -3,5 +3,17 @@ package prob2;
 public class Prob2 {
 	public static void main(String[] args) {
 		/* 코드 작성 */
+		int count=0;
+		int a=0;
+		while(true) {
+			a+=1;
+			for(int i=0; i<10; i++) {
+				System.out.print((a+i)+" ");
+			}
+			System.out.println();
+			if(a==5) {
+				break;
+			}
+		}
 	}
 }
--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -5,11 +5,28 @@ import java.util.Scanner;
 public class Prob3 {
 	
 	public static void main(String[] args) {
-
+		int number=0;
+		int share=0;
+		int odd_num=1;
+		int sum=0;
 		Scanner scanner = new Scanner(System.in);
 
 		/* 코드 작성 */
+		System.out.print("숫자를 입력하세요: ");
+		number=scanner.nextInt();
 		
+		if(number%2==1) {
+			share=number/2;
+			for(int i=0; i<=share; i++) {
+				sum= sum+odd_num;
+				odd_num+=2;
+				
+			}
+			System.out.println("결과 값: "+sum);
+		}
+		else if(number%2==0) {
+			
+		}
 		scanner.close();
 	}
 }
