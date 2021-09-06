99a20f5c4f5a504502e2fd4b41123b2618fe5210
user <user@218.154.207.106>
2021-09-06 18:46:53 +0900

    first commit //첫 커밋과 푸시

---
+++ practice01/src/main/java/prob1/Prob1.java
@@ -0,0 +1,16 @@
+package prob1;
+
+import java.util.Scanner;
+
+public class Prob1 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner( System.in );
+		
+		/* 코드 작성 */
+		int number;
+		System.out.print("수를 입력하세요 : ");
+		number=scanner.nextInt();
+		
+		scanner.close();
+	}
+}
